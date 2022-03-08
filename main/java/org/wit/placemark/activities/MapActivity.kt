6aaf31903e06f6aa42c6c43f777f50145561afe9
dermotgrace <98397084+dermotgrace@users.noreply.github.com>
2022-03-08 19:09:11 +0000

    Showing google map

--- main/java/org/wit/placemark/activities/MapActivity.kt
+++ main/java/org/wit/placemark/activities/MapActivity.kt
@@ -19,19 +19,19 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        i("in map 1")
+
        super.onCreate(savedInstanceState)
-         i("in map 2")
+
         binding = ActivityMapBinding.inflate(layoutInflater)
-        i("in map 3")
+
         setContentView(binding.root)
-        i("in map 4")
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
-        i("in map 5")
+
         mapFragment.getMapAsync(this)
-        i("in map 6")
+
     }
 
     /**
@@ -44,15 +44,15 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        i("in map 7")
+
         mMap = googleMap
-        i("in map 8")
+
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        i("in map 9")
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        i("in map 10")
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        i("in map 11")
+        val sydney = LatLng(52.245696, -7.139102)
+
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Waterford"))
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wit, 16f))
+
     }
 }
